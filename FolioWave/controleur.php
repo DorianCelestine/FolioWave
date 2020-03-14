34e886d9b76523fc52ae7967743514977ec0a98b
eung-paris8 <39116117+eung-paris8@users.noreply.github.com>
2020-03-14 16:48:22 +0100

    MàJ

---
+++ FolioWave/controleur.php
@@ -0,0 +1,21 @@
+<?php
+	require 'vue.php';
+	require 'modele.php';
+	class controleur{
+		private $vue;
+		private $mod;
+
+		public function __construct(){
+			$this->vue=new Vue();
+			$this->mod=new Modele();
+		}
+
+		public function getAff(){
+			return $this->vue->getBody();
+		}
+		
+		public function sendMail(){
+		    $this->mod->sendMail();
+		}
+	}
+?>
